bbd847fd497bbf3adaa4eb9fc888129863653a3e
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-02 18:25:42 -0600

    Revert "Update ipc_test.py"
    This reverts commit 58861a286757b803a379d8659ce9ddade807cb47.

--- scrips/ipc_test.py
+++ scrips/ipc_test.py
@@ -104,6 +104,89 @@ reporte.agregar_subcapitulo(
     data=subcap_data[0],
     opciones_grafico={"precision":1}
 )
+# capitulo 2
+reporte.agregar_capitulo(
+    titulo="Variables exógenas"
+)
+subcap_data = datos.indice_precio_alimentos()
+reporte.agregar_subcapitulo(
+    titulo="Precio internacional de los alimentos",
+    titulo_grafico="Índice de precios de los alimentos de la FAO",
+    descripcion_grafico="Indicador internacional, serie histórica, adimensional",
+    descripcion=subcap_data[1],
+    fuente="FAO",
+    tipo_grafico="lineal",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2, "Q4Etiquetas":True}
+)
+subcap_data = datos.petroleo()
+reporte.agregar_subcapitulo(
+    titulo="Precio del pretróleo",
+    titulo_grafico="Precio promedio mensual del barril del petróleo",
+    descripcion_grafico="Indicador internacional, serie histórica, en dólares por barril",
+    descripcion=subcap_data[1],
+    fuente="Federal Reserve Economic Data",
+    tipo_grafico="lineal",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2, "Q4Etiquetas":True}
+)
+subcap_data = datos.cambio_quetzal()
+reporte.agregar_subcapitulo(
+    titulo="Cambio del quetzal",
+    titulo_grafico="Tipo de cambio nominal promedio",
+    descripcion_grafico="República de Guatemala, serie histórica, en quetzales por dólar estadounidense",
+    descripcion=subcap_data[1],
+    fuente="Banco de Guatemala",
+    tipo_grafico="lineal",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2, "Q4Etiquetas":True}
+)
+subcap_data = datos.tasa_interes()
+reporte.agregar_subcapitulo(
+    titulo="Tasa de interés",
+    titulo_grafico="Tasas de interés activa bancaria",
+    descripcion_grafico="República de Guatemala, serie histórica, en porcentaje",
+    descripcion=subcap_data[1],
+    fuente="Banco de Guatemala",
+    tipo_grafico="lineal",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2, "Q4Etiquetas":True}
+)
+subcap_data = datos.ipc_usa()
+reporte.agregar_subcapitulo(
+    titulo="Índice de Precios al Consumidor de EE.UU.",
+    titulo_grafico="Variación interanual del IPC de Estados Unidos de América",
+    descripcion_grafico="Estados Unidos de América, serie histórica, en porcentaje",
+    descripcion=subcap_data[1],
+    fuente="FRED",
+    tipo_grafico="lineal",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2, "Q4Etiquetas":True}
+)
+subcap_data = datos.ipc_mex()
+reporte.agregar_subcapitulo(
+    titulo="Índice de Precios al Consumidor de México",
+    titulo_grafico="Variación interanual del IPC de México",
+    descripcion_grafico="Estados Unidos Mexicanos, serie histórica, en porcentaje",
+    descripcion=subcap_data[1],
+    fuente="FRED",
+    tipo_grafico="lineal",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2, "Q4Etiquetas":True}
+)
+'''
+subcap_data = datos.inflacion_CA_RD_MEX()
+reporte.agregar_subcapitulo(
+    titulo="Inflación en Centro América, República Dominicana y México",
+    titulo_grafico="Tasa de variación interanual del IPC de los países Centroamericanos, República Dominicana y México",
+    descripcion_grafico="Centro América, República Dominicana y México, meses seleccionados, en porcentaje",
+    descripcion=subcap_data[1],
+    fuente="INE",
+    tipo_grafico="tabla",
+    data=subcap_data[0],
+    opciones_grafico={"precision":2}
+)
+'''
 
 # capitulo 3
 reporte.agregar_capitulo(
