e436112297e3880844f3008781237a6cc2af3d7c
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-06-07 20:50:27 -0600

    Update tiempo.bat

--- tiempo.bat
+++ tiempo.bat
@@ -1,6 +1,6 @@
 @echo off
 FOR /L %%i IN (1,1,100) DO (
-    mkdir resultados
+    mkdir resultado
     cd resultado
     speedtest-cli --no-upload --json > velocidad_internet_%%i.json
     docker build --no-cache -t ipc_dev C:\Users\laalvarado\Documents\GitHub\INE_docker_ipc > build_time_%%i.txt 
